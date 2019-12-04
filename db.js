f76bd82727be0d386f6190b5d6847dc8013bd91f
amleonerd <55100142+amleonerd@users.noreply.github.com>
2019-12-04 09:48:18 -0500

    Tuesdays work

--- db.js
+++ db.js
@@ -6,3 +6,11 @@ connection.connect((err) => {
         console.log("database is not connected"+err.message);
     }
 });
+
+var  get_all_inventory  = updateInventory; 
+console.log("we have this many" + updateInventory); 
+
+var  set_new_item = x + y;
+console.log("your item was successfull added to our database" + updateInventory);
+
+module.exports.set_new_item = set_new_item; 
